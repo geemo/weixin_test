ab5aba78c4108546d8be989711b4f83429073298
geemo <153330685@qq.com>
2016-08-08 16:45:07 +0800

    素材管理库实现

--- test/material_test.js
+++ test/material_test.js
@@ -2,11 +2,27 @@
 
 const Material = require('../lib/material.js');
 const fs = require('fs');
+const path = require('path');
 
 const material = new Material();
 
 material
-	.upload({
-		'media': fs.createReadStream('../material/read.png')
-	}, 'image')
+	.upload(path.resolve('../material/read.png'), 'image')
 	.then(data => console.dir(data), err => console.error(err));
+
+// material
+// 	.uploadImg(path.resolve('../material/read.png'), 'image')
+// 	.then(data => console.dir(data), err => console.error(err));
+
+material
+	.getMaterialCount()
+	.then(data => console.dir(data), err => console.error(err));
+
+// material
+// 	.batchGetMaterial({type: 'image', offset: 0, count: 1})
+// 	.then(data => {
+// 		console.dir(data);
+// 	}, err => {
+// 		console.error(err);
+// 	});
+// 	
